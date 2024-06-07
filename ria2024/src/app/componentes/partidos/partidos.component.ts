0a5b375b0a8c4cbe8ab540049ffdff68a413befe
Cristian <89285428+Cristiangonzz@users.noreply.github.com>
2024-06-07 19:32:50 -0300

    Se agregaron las clases para filtros

--- ria2024/src/app/componentes/partidos/partidos.component.ts
+++ ria2024/src/app/componentes/partidos/partidos.component.ts
@@ -3,7 +3,7 @@ import { IAllMatchesDomain } from '../../dominio/ApiMatches/IAllMatches';
 import { ApiService } from '../../servicios/api.service';
 import { IMatcheDomain } from '../../dominio/ApiMatches/IMatcheDomain';
 
-export interface IApuesta{
+export interface IApuesta {
   home: number;
   away: number;
 }
@@ -11,31 +11,60 @@ export interface IApuesta{
 @Component({
   selector: 'app-partidos',
   templateUrl: './partidos.component.html',
-  styleUrl: './partidos.component.css'
+  styleUrl: './partidos.component.css',
 })
 export class PartidosComponent implements OnInit {
   matches: IAllMatchesDomain = {} as IAllMatchesDomain;
 
   apuestas: IApuesta[] = [];
   showModal = false;
-  currentApuesta : IApuesta = {home:0,away:0};
+  currentApuesta: IApuesta = { home: 0, away: 0 };
   currentIndex = 0;
+  dateFrom!: string;
+  dateTo!: string;
 
-  summarieBetdarar!: any;
+  matchsApuesta: IMatcheDomain = {} as IMatcheDomain; //Partidos el cual el cliente selecciona para apostar
 
-  matchsApuesta: IMatcheDomain = {} as IMatcheDomain;//Partidos el cual el cliente selecciona para apostar
+  constructor(private apiService: ApiService) {}
 
-  
-  constructor(
-    private apiService: ApiService,
-  ) { }
-  
   ngOnInit(): void {
-   this.apiService.ObtenerPartidos().subscribe((match : any) => {
-    this.summarieBetdarar = match;
-   })
+    this.apiService.ObtenerPartidos().subscribe((match: IAllMatchesDomain) => {
+      if (match != null) {
+        this.matches = match;
+        console.log('Mis partidos:' + JSON.stringify(this.matches, null, 2));
+      } else {
+        console.log('Fallo la consulta a la api');
+      }
+    });
+  }
+  partidosPorFechas(): void {
+    const dateFrom = this.formatDate(this.parseDateString(this.dateFrom));
+    const dateTo = this.formatDate(this.parseDateString(this.dateTo));
+    console.log('Fecha desde: ' + dateFrom);
+    console.log('Fecha hasta: ' + dateTo);
+    this.apiService
+      .ObtenerPartidosPorFechas(dateFrom, dateTo)
+      .subscribe((match: IAllMatchesDomain) => {
+        if (match != null) {
+          this.matches = match;
+          console.log('Mis partidos:' + JSON.stringify(this.matches, null, 2));
+        } else {
+          console.log('Fallo la consulta a la api');
+        }
+      });
   }
 
+  parseDateString(dateString: string): Date {
+    const [year, month, day] = dateString.split('-').map(Number);
+    return new Date(year, month - 1, day);
+  }
+
+  formatDate(date: Date): string {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0');
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+  }
   openBetDialog(index: number): void {
     this.currentIndex = index;
     this.matchsApuesta = this.matches.matches[index];
@@ -50,8 +79,6 @@ export class PartidosComponent implements OnInit {
   confirmBet(): void {
     this.apuestas[this.currentIndex] = { ...this.currentApuesta };
     this.closeBetDialog();
-    console.log("Todas las apuestas: " + this.apuestas);
+    console.log('Todas las apuestas: ' + this.apuestas);
   }
-
-  
 }
--- ria2024/src/app/servicios/api.service.ts
+++ ria2024/src/app/servicios/api.service.ts
@@ -4,21 +4,33 @@ import { IAllMatchesDomain } from '../dominio/ApiMatches/IAllMatches';
 import { Observable } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  private apiUrl = '/api/v4/matches';
+  private apiToken = '5e5f4b403ed749dcb7065634af5e8dc4';
 
-  private apiUrl = 'https://api.sportradar.com/soccer/trial/v4/en/schedules/live/summaries?api_key=i0kJloOZsr5BZVu7p0IsA9cF0oEXXP7z6Dnnz83s';
-  //private apiToken = '5e5f4b403ed749dcb7065634af5e8dc4';
+  constructor(private http: HttpClient) {}
 
-  constructor(private http: HttpClient) { }
-
-  public ObtenerPartidos(): Observable<IAllMatchesDomain>{
+  public ObtenerPartidos(): Observable<IAllMatchesDomain> {
     const headers = new HttpHeaders({
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-      'Access-Control-Allow-Headers': 'Content-Type, Authorization'
-    });  
-    return this.http.get<IAllMatchesDomain>(this.apiUrl,{headers});
+      'X-Auth-Token': this.apiToken,
+    });
+    return this.http.get<IAllMatchesDomain>(this.apiUrl, { headers });
+  }
+
+  public ObtenerPartidosPorFechas(
+    dateFrom: string,
+    dateTo: string
+  ): Observable<IAllMatchesDomain> {
+    const headers = new HttpHeaders({
+      'Access-Control-Allow-Origin': '*',
+      'X-Auth-Token': this.apiToken,
+    });
+    return this.http.get<IAllMatchesDomain>(
+      this.apiUrl + '?dateFrom=' + dateFrom + '&dateTo=' + dateTo,
+      { headers }
+    );
   }
 }
